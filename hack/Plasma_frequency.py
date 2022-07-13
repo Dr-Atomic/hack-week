f459b8197af30d7fa1b6ed3e121f4321059a5418
Dr-Atomic <109099125+Dr-Atomic@users.noreply.github.com>
2022-07-13 15:01:10 -0500

    Update Plasma_frequency.py

--- hack/Plasma_frequency.py
+++ hack/Plasma_frequency.py
@@ -1,24 +1,14 @@
-plasmapy.formulary.frequencies.plasma_frequency(n: Unit('1 / m3'), particle: Particle, z_mean=None, to_hz=False)
-
 from astropy import units as u
 
-plasma_frequency(1e19*u.m**-3, particle='p')
-<Quantity 4.16329...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='D+')
-<Quantity 2.94462...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='D0')
-<Quantity 2.94462...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='D-')
-<Quantity 2.94462...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='T+')
-<Quantity 2.94462...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='T0')
-<Quantity 2.94462...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='T-')
-<Quantity 2.94462...e+09 rad / s>
-plasma_frequency(1e19*u.m**-3, particle='p', to_hz=True)
-<Quantity 6.62608...e+08 Hz>
-plasma_frequency(1e19*u.m**-3, 'e-', to_hz=True)
-<Quantity 2.83930...e+10 Hz>
-
+def plasma__frequency(1e19*u.m**-3, particle='p'):
+    """
+    Calculate the plasma frequency of a plasma with the given parameters.
+    """
+    return (B0 / B) * (Te / ne)
+    
+def plasma__frequency__SI(1e19*u.m**-3, particle='p'):
+    """
+    Calculate the plasma frequency of a plasma with the given parameters.
+    """
+    return (B0 / B) * (Te / ne)
 
